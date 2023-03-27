e0747d92414789de032916876e6dee2957c38110
Aasif Antue <aasifantue@Antues-Mac-Mini.local>
2023-03-27 20:18:04 +0600

    icondone

--- src/assets/Component/Product/Product.jsx
+++ src/assets/Component/Product/Product.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
+
+
 const Product = (props) => {
+    console.log(props);
     const {img, name, seller, quantity, price, ratings} = props.product;
+
+    const handleAddToCart = props.handleAddToCart; 
+
     return (
         <div className='product'>
             <img src= {img} alt="" />
@@ -12,7 +20,11 @@ const Product = (props) => {
             <p className='product-seller'>Manufacturer: {seller}</p>
             <p className='product-rating'>Rating: {ratings}</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} 
+            className='btn-cart'>
+                Add to Cart
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>
             
         </div>
     );
